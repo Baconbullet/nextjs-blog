9dcd32b28d41e88404d9d0f8c55d4c5eed876230
Baconbullet <kgutierrez626@gmail.com>
2023-05-12 12:56:32 -0700

    Header component added

---
+++ components/header.js
@@ -0,0 +1,17 @@
+
+
+export default function Header() {
+    return (
+        <header>
+            <h1>My Blog</h1>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/blog-entries">Blogs</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact Me</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
